9e861fecb22484bbd7d0780280399f0d543067a5
Rogerio Teixeira <codificarciencias@gmail.com>
2021-03-30 23:04:44 -0300

    Cameras
    configuração de zoom e foco multi-câmera.

--- viajando.gd
+++ viajando.gd
@@ -4,19 +4,27 @@ var dist = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Refer/GlobCam.make_current()
 	pass
 
 func _process(delta):
-	
+	# mostra velocidade da nave
 	if $Nave.velocidade <= 0:
-		$Canvlayer/LbNaveVel.text = 'Vel = ' + str(int($Nave.velocidade*-1))
+		$Controle/LbNaveVel.text = 'Vel = ' + str(int($Nave.velocidade*-1))
 	else:
-		$Canvlayer/LbNaveVel.text = 'Vel = ' + str(int($Nave.velocidade))
+		$Controle/LbNaveVel.text = 'Vel = ' + str(int($Nave.velocidade))
 	
+	# Controle da camera global
 	$Refer.position.x = ($Nave.position.x - $Terra.position.x)/2+$Terra.position.x
 	if $Refer.position.x >= 400:
-		dist = ($Nave.position.x - $Terra.position.x)/900
-		$Refer/Camera2D.zoom.x = dist
-		$Refer/Camera2D.zoom.y = dist
-		$Refer.scale = Vector2(7-dist/8000, 7-dist/8000)
+		dist = ($Nave.position.x - $Terra.position.x)/1100
+		$Refer/GlobCam.zoom.x = dist
+		$Refer/GlobCam.zoom.y = dist
+		$Refer.scale = Vector2(0.5+dist*2, 0.5+dist*2)
+		print(dist)
 	
+	if Input.is_action_just_pressed("ui_up"):
+		if $Refer/GlobCam.current:
+			$Nave/NavCam.make_current()
+		else:
+			$Refer/GlobCam.make_current()
